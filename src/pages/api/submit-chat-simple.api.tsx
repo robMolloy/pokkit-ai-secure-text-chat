a6a916fd3b41065b48501209ffe892cbc218099f
robmolloy <robmolloy@hotmail.co.uk>
2025-07-30 19:26:04 +0100

    using new callAnthropic version

--- src/pages/api/submit-chat-simple.api.tsx
+++ src/pages/api/submit-chat-simple.api.tsx
@@ -1,4 +1,8 @@
-import { createAnthropicMessage, TAnthropicMessage } from "@/modules/providers/anthropicApi";
+import {
+  callAnthropic,
+  createAnthropicMessage,
+  TAnthropicMessage,
+} from "@/modules/providers/anthropicApi";
 import Anthropic from "@anthropic-ai/sdk";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -9,34 +13,6 @@ const delay = async (x: number) => {
 };
 
 const anthropic = new Anthropic({ apiKey: process.env.ANTHROPIC_KEY });
-
-const callAnthropic = async (p: {
-  anthropicInstance: Anthropic;
-  messages: TAnthropicMessage[];
-  onNewChunk: (x: string) => void;
-}) => {
-  try {
-    const responseChunks: string[] = [];
-    const chunks = await p.anthropicInstance.messages.create({
-      model: "claude-3-5-haiku-20241022",
-      max_tokens: 5000,
-      messages: p.messages,
-      stream: true,
-    });
-
-    for await (const chunk of chunks) {
-      if (chunk.type === "content_block_delta" && "text" in chunk.delta) {
-        await delay(25);
-        responseChunks.push(chunk.delta.text);
-        p.onNewChunk(chunk.delta.text);
-      }
-    }
-
-    return { success: true, data: responseChunks.join("") } as const;
-  } catch (error) {
-    return { success: false, error } as const;
-  }
-};
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse<unknown>) => {
   res.setHeader("Content-Type", "text/plain; charset=utf-8");
